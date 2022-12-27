a043e78eeb095070c25887a8113e4654c7443caa
Joe Jordan <dev@ignitesoft.com>
2022-12-27 01:01:31 -0500

    rearrange storage vars

--- src/NFT.sol
+++ src/NFT.sol
@@ -4,15 +4,17 @@ pragma solidity ^0.8.17;
 import { BytesLib } from "solidity-bytes-utils/BytesLib.sol";
 
 contract NFT {
-    uint256 public maxSupply = 5000;
 
     mapping(uint256 => bytes) public allSeedData;
 
-    // size of a single packed seed entry
-    uint8 public constant SEED_ENTRY_SIZE = 32;
+    // total supply of tokens to generate
+    uint256 public maxSupply = 1000;
 
     // size of a chunk of seed data stored in our allSeedData mapping
-    uint256 public constant SEED_CHUNK_SIZE = 250;
+    uint256 public constant SEED_CHUNK_SIZE = 500;
+
+    // size of a single packed seed entry
+    uint8 public constant SEED_ENTRY_SIZE = 32;
 
     /// @notice sets all seed data for the project
     function setSeedData(uint8 index, bytes calldata _seedData) public {
@@ -42,27 +44,6 @@ contract NFT {
     function getSeedDataIndex(uint256 id) public view returns (uint256) {
         require(id < maxSupply, "ID_OUT_OF_RANGE");
         return id / SEED_CHUNK_SIZE;
-        // if (id < 500) {
-        //     return 0;
-        // } else if (id < 1000) {
-        //     return 1;
-        // } else if (id < 1500) {
-        //     return 2;
-        // } else if (id < 2000) {
-        //     return 3;
-        // } else if (id < 2500) {
-        //     return 4;
-        // } else if (id < 3000) {
-        //     return 5;
-        // } else if (id < 3500) {
-        //     return 6;
-        // } else if (id < 4000) {
-        //     return 7;
-        // } else if (id < 4500) {
-        //     return 8;
-        // } else {
-        //     return 9;
-        // }
     }
 
     /// @notice helper to get the bytes array startIndex based upon the desired token id
